04af69d0c85da14fb6b59f9520d51f692310d22d
camelone49 <hisham.farid@rwth-aachen.de>
2025-07-26 10:22:49 +0200

    loop thing

--- hishamstest/openvlatest.py
+++ hishamstest/openvlatest.py
@@ -8,6 +8,12 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 
+def SendPosToRobot(dSetpointVector):
+    # Placeholder function to send position to robot
+    # Implement the actual logic to send the setpoint vector to your robot
+    #needs inverse dynamics
+    return
+
 # Load model & processor
 # pip install transformers==4.41.2 --force-reinstall --no-cache-dir  
 
@@ -21,29 +27,46 @@ vla = AutoModelForVision2Seq.from_pretrained(
     trust_remote_code=True
 ).to("cuda:0")
 
-
-pil_img = Image.open("frame_plot.jpg").convert("RGB")
-
-# Convert PIL Image to NumPy array (RGB)
-frame_rgb = np.array(pil_img)
-
-# If you need OpenCV BGR format instead of RGB:
-frame_bgr = cv2.cvtColor(frame_rgb, cv2.COLOR_RGB2BGR)
-
-# Now frame_rgb or frame_bgr is usable like a frame from cv2.VideoCapture
-
-# Convert BGR (OpenCV) to RGB (PIL)
-image = Image.fromarray(frame_rgb)
-
-# Prompt (insert real instruction)
-print("Processing input...")
 instruction = "pick up the white cup"
-prompt = f"In: What action should the robot take to {instruction}?\nOut:"
 
-# Process input and predict action
-inputs = processor(prompt, image).to("cuda:0", dtype=torch.bfloat16)
-action = vla.predict_action(**inputs, unnorm_key="bridge_orig", do_sample=False)
+bSetpointReached = False #variable to control the loop
+dSetpointVector = None #variable to store the setpoint vector
 
-# Output
-print("The action for the given prompt is:")
-print("Predicted action:", action)
+while True:
+
+    if bSetpointReached == True:
+        pil_img = Image.open("frame_plot.jpg").convert("RGB") # REPLACE WITH YOUR VIDEO SOURCE
+
+        # Convert PIL Image to NumPy array (RGB)
+        frame_rgb = np.array(pil_img)
+
+        # If you need OpenCV BGR format instead of RGB:
+        frame_bgr = cv2.cvtColor(frame_rgb, cv2.COLOR_RGB2BGR)
+
+        # Now frame_rgb or frame_bgr is usable like a frame from cv2.VideoCapture
+
+        # Convert BGR (OpenCV) to RGB (PIL)
+        image = Image.fromarray(frame_rgb)
+
+        # Prompt (insert real instruction)
+        print("Processing input...")
+        prompt = f"In: What action should the robot take to {instruction}?\nOut:"
+
+        # Process input and predict action
+        inputs = processor(prompt, image).to("cuda:0", dtype=torch.bfloat16)
+        dSetpointVector = vla.predict_action(**inputs, unnorm_key="bridge_orig", do_sample=False)
+        
+        SendPosToRobot(dSetpointVector) #IMPLEMENT THIS FUNCTION, its a function to send the setpoint vector to the robot
+
+        # Output
+        print("The action for the given prompt is:")
+        print("Predicted action:", dSetpointVector)
+        
+    else: #if the setpoint is not reached, do nothing, the robot is still moving
+        bSetpointReached = False
+        
+        dRobot_position = None # Replace with actual robot position retrieval logicq
+        
+        if dRobot_position == dSetpointVector:
+            setpoint_reached = True
+        continue
---
+++ hishamstest/smolvlmtest.py
@@ -0,0 +1,89 @@
+from transformers import AutoProcessor, AutoModelForImageTextToText
+import torch
+
+model_path = "HuggingFaceTB/SmolVLM2-500M-Video-Instruct"
+processor = AutoProcessor.from_pretrained(model_path)
+model = AutoModelForImageTextToText.from_pretrained(
+    model_path,
+    torch_dtype=torch.bfloat16,
+    _attn_implementation="eager"
+).to("cuda")
+
+
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "image", "url": "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/bee.jpg"},
+            {"type": "text", "text": "Can you describe this image?"},
+        ]
+    },
+]
+
+inputs = processor.apply_chat_template(
+    messages,
+    add_generation_prompt=True,
+    tokenize=True,
+    return_dict=True,
+    return_tensors="pt",
+).to(model.device, dtype=torch.bfloat16)
+
+generated_ids = model.generate(**inputs, do_sample=False, max_new_tokens=64)
+generated_texts = processor.batch_decode(
+    generated_ids,
+    skip_special_tokens=True,
+)
+print(generated_texts[0])
+
+
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "video", "path": "path_to_video.mp4"},
+            {"type": "text", "text": "Describe this video in detail"}
+        ]
+    },
+]
+
+inputs = processor.apply_chat_template(
+    messages,
+    add_generation_prompt=True,
+    tokenize=True,
+    return_dict=True,
+    return_tensors="pt",
+).to(model.device, dtype=torch.bfloat16)
+
+generated_ids = model.generate(**inputs, do_sample=False, max_new_tokens=64)
+generated_texts = processor.batch_decode(
+    generated_ids,
+    skip_special_tokens=True,
+)
+
+print(generated_texts[0])
+
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "video", "path": "path_to_video.mp4"},
+            {"type": "text", "text": "Describe this video in detail"}
+        ]
+    },
+]
+
+inputs = processor.apply_chat_template(
+    messages,
+    add_generation_prompt=True,
+    tokenize=True,
+    return_dict=True,
+    return_tensors="pt",
+).to(model.device, dtype=torch.bfloat16)
+
+generated_ids = model.generate(**inputs, do_sample=False, max_new_tokens=64)
+generated_texts = processor.batch_decode(
+    generated_ids,
+    skip_special_tokens=True,
+)
+
+print(generated_texts[0])
